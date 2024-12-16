08764bda19a41e479ca26225f468b35d3dac062c
Petr <psfinaki@users.noreply.github.com>
2024-12-16 16:29:03 +0100

    osgn

--- tests/FSharp.Compiler.Service.Tests/Dump3.fs
+++ tests/FSharp.Compiler.Service.Tests/Dump3.fs
@@ -107,7 +107,7 @@ let GetSignatureData1() =
         (byte)0;  // pubpath table
         (byte)0;  // n lerefe table
         (byte)0;  // simple type tables
-        (byte)33; // phase1bytes
+        (byte)57; // phase1bytes
         (byte)0;
         (byte)0;  // tyar_spec
         (byte)0;  // logical name
@@ -137,6 +137,30 @@ let GetSignatureData1() =
         (byte)0;  // entity flags
         (byte)0;
         (byte)0;  // more opt data
+        (byte)0;  // osgn
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
+        (byte)0;
         (byte)0;
         (byte)0;
         (byte)0;
