1edfb010db26b93ffe5bca764165729969a9ed0c
Petr <psfinaki@users.noreply.github.com>
2024-12-16 16:45:14 +0100

    quotations

--- tests/FSharp.Compiler.Service.Tests/Dump3.fs
+++ tests/FSharp.Compiler.Service.Tests/Dump3.fs
@@ -107,7 +107,7 @@ let GetSignatureData1() =
         (byte)0;  // pubpath table
         (byte)0;  // n lerefe table
         (byte)0;  // simple type tables
-        (byte)63; // phase1bytes
+        (byte)64; // phase1bytes
         (byte)0;
         (byte)0;  // tyar_spec
         (byte)0;  // logical name
@@ -171,6 +171,7 @@ let GetSignatureData1() =
         (byte)3;  // exception representation
         (byte)0;  // extra space
         (byte)0;  // working dir
+        (byte)0;  // use quotations
     |]
 
     let byteReaderA () = 
