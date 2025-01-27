867c0b465762a1b6bc23f40f6008ce591e7756e4
Petr <psfinaki@users.noreply.github.com>
2025-01-27 16:46:15 +0100

    up

--- tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/Recompilation.fs
+++ tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/Recompilation.fs
@@ -16,8 +16,8 @@ type Recompilation() =
     [<InlineData "">]
     [<InlineData "()">]
     [<InlineData "42">]
-    // [<InlineData "exit 0">]
-    // [<InlineData "printfn \"Hello world!\"">]
+    [<InlineData "exit 0">]
+    [<InlineData "printfn \"Hello world!\"">]
     let ``Recompiles using restored TC info`` (code: string) =
         let tempPath = $"{getTemporaryFileName()}.fsx"
         
@@ -27,6 +27,7 @@ type Recompilation() =
             FsxFromPath tempPath
             |> withReuseTcResults
             |> withOptions [ "--compressmetadata-" ]
+            |> withOptions [ "--optimize-" ]
 
         let r1 =
             cUnit
