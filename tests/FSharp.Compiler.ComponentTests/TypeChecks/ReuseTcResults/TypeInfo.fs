6340e05c716574d08b91d44d885152f073049684
Petr <psfinaki@users.noreply.github.com>
2025-01-27 11:12:09 +0100

    Update TypeInfo.fs

--- tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/TypeInfo.fs
+++ tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/TypeInfo.fs
@@ -9,14 +9,13 @@ open Xunit
 
 open TestFramework
 
-
+[<Collection(nameof NotThreadSafeResourceCollection)>]
 type TypeInfo() =
 
-    [<Fact>]
-    let ``Recompilation 1``() =
+    let test (code: string) =
         let tempPath = $"{getTemporaryFileName()}.fsx"
         
-        File.WriteAllText(tempPath, "42") 
+        File.WriteAllText(tempPath, code) 
 
         let cUnit =
             FsxFromPath tempPath
@@ -38,26 +37,21 @@ type TypeInfo() =
         Assert.Equal(r1, r2)
 
     [<Fact>]
-    let ``Recompilation 2``() =
-        let tempPath = $"{getTemporaryFileName()}.fsx"
+    let ``Empty file``() =
+        test ""
+
+    [<Fact>]
+    let ``Empty function``() =
+        test "()"
         
-        File.WriteAllText(tempPath, "printfn \"Hello world!\"") 
+    [<Fact>]
+    let ``42``() =
+        test "42"
+        
+    [<Fact>]
+    let ``exit``() =
+        test "exit 0"
 
-        let cUnit =
-            FsxFromPath tempPath
-            |> withReuseTcResults
-            |> withOptions [ "--compressmetadata-" ]
-
-        let r1 =
-            cUnit
-            |> compileExisting
-            |> shouldSucceed
-            |> fun r -> ILChecker.generateIL r.Output.OutputPath.Value []
-
-        let r2 =
-            cUnit
-            |> compileExisting
-            |> shouldSucceed
-            |> fun r -> ILChecker.generateIL r.Output.OutputPath.Value []
-
-        Assert.Equal(r1, r2)
+    [<Fact>]
+    let ``hello world``() =
+        test "printfn \"Hello world!\""
