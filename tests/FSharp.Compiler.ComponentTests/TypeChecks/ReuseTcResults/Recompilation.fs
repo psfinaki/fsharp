9f43bb2a4cb6d748d93670d33ce3e125b366fa89
Petr Semkin <psemkin@microsoft.com>
2025-02-03 17:09:30 +0100

    tests

--- tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/Recompilation.fs
+++ tests/FSharp.Compiler.ComponentTests/TypeChecks/ReuseTcResults/Recompilation.fs
@@ -22,17 +22,23 @@ type Recompilation() =
     [<InlineData "namespace Test">]
     [<InlineData """namespace Test
 module M1 = ()""">]
-    //[<InlineData s"let x = 42">]
-    //[<InlineData "let f() = 42">]
     [<InlineData "let f x = x">]
-    //[<InlineData "let f x = x * x">]
+    [<InlineData """let f1 x = x
+let rec f2 x = x""">]
+    [<InlineData "let rec f x = x">]
     [<InlineData "type T = int">]
+    //[<InlineData "let x = []">]
+
+    //[<InlineData "let f x = x * x">]
+    //[<InlineData "let x = 42">]
+    //[<InlineData "let rec f x = x">]
+    //[<InlineData "let f() = 42">]
     //[<InlineData "type DU = A of int">]
     //[<InlineData "type DU = A of int | B of string">]
     //[<InlineData "type R = { v: int }">]
     //[<InlineData "Some 42">]
     //[<InlineData "None">]
-
+    //[<InlineData "let f = function | _ -> 42">]
     let ``Recompiles using restored TC info`` (code: string) =
         let tempPath = $"{getTemporaryFileName()}.fsx"
         
